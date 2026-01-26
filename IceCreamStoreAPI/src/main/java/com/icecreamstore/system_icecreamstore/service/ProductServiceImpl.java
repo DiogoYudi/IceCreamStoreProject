41ad08c216463671406a899f21329a0c0daec5c8
DIOGO YUDI YUKUI <112355@unoeste.br>
2026-01-26 17:18:32 -0300

    feat: implement backend CRUD for products

--- IceCreamStoreAPI/src/main/java/com/icecreamstore/system_icecreamstore/service/ProductServiceImpl.java
+++ IceCreamStoreAPI/src/main/java/com/icecreamstore/system_icecreamstore/service/ProductServiceImpl.java
@@ -2,7 +2,6 @@ package com.icecreamstore.system_icecreamstore.service;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.BeanUtils;
