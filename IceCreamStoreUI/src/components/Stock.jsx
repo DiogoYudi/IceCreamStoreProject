4777766d99e2a03a1876f7d1bc0ca419cfa7504b
DIOGO YUDI YUKUI <112355@unoeste.br>
2026-02-23 17:22:33 -0300

    feat: implement modal to increase stock quantity

--- IceCreamStoreUI/src/components/Stock.jsx
+++ IceCreamStoreUI/src/components/Stock.jsx
@@ -1,3 +1,4 @@
+import { X } from "lucide-react";
 import { useState } from "react";
 
 function Stock() {
@@ -6,12 +7,16 @@ function Stock() {
   const [appliedFilter, setAppliedFilter] = useState("");
   const [selectedOption, setSelectedOption] = useState(null);
   const [selectedNewStock, setSelectedNewStock] = useState(null);
+  const [selectedAddStock, setSelectedAddStock] = useState(null);
+  const [selectedItem, setSelectedItem] = useState(null);
+  const [newQuantity, setNewQuantity] = useState(null);
   const [newType, setNewType] = useState("");
   const [newStock, setNewStock] = useState({
     type: "",
     flavour: "",
     quantity: "",
   });
+
   const options = [
     { code: 1, name: "Balde", isDisabled: false },
     { code: 2, name: "Pote 2L", isDisabled: false },
@@ -27,33 +32,48 @@ function Stock() {
     { id: 5, name: "Palito", quantity: 15 },
   ];
 
-  const filteredItems = items.filter((item) => {
-    const matchesSelect = filter ? item.name === filter : true;
+  // Separando os itens com e sem sabor
+  const itemsWithFlavour = items.filter((item) => item.flavour);
+  const itemsWithoutFlavour = items.filter((item) => !item.flavour);
 
+  // Aplicando filtros
+  const filteredWithFlavour = itemsWithFlavour.filter((item) => {
+    const matchesSelect = filter ? item.name === filter : true;
     const matchesSearch =
       appliedFilter === ""
         ? true
         : item.name.toLowerCase().includes(appliedFilter.toLowerCase()) ||
           item.flavour.toLowerCase().includes(appliedFilter.toLowerCase());
-
     return matchesSelect && matchesSearch;
   });
 
-  const addStock = () => {};
+  const filteredWithoutFlavour = itemsWithoutFlavour.filter((item) => {
+    return filter ? item.name === filter : true;
+  });
+
+  const addStock = () => {
+    // Função de adicionar estoque (a implementar)
+  };
 
   return (
     <div className="ml-0 md:ml-40 h-[calc(100vh-40px)] bg-purple-backgroundbody flex flex-col">
+      {/* Header */}
       <div className="bg-gray-backgroundbody h-[10%] flex justify-between items-center px-5">
         <h1 className="text-purple-950 font-bold text-lg opacity-85">
           Estoque
         </h1>
         <button
-          onClick={(e) => {setSelectedNewStock(e); setSelectedOption(options[0])}}
+          onClick={(e) => {
+            setSelectedNewStock(e);
+            setSelectedOption(options[0]);
+          }}
           className="bg-purple-options text-gray-300 rounded-sm shadow w-[10%] h-[70%] cursor-pointer hover:scale-110"
         >
           Novo Estoque
         </button>
       </div>
+
+      {/* Filtros */}
       <div className="flex flex-col items-center pt-7 gap-7 relative">
         <div className="flex w-[90%] h-10 justify-between">
           <button
@@ -81,121 +101,205 @@ function Stock() {
             value={searchTerm}
             onChange={(e) => {
               const value = e.target.value;
-              setSearchTerm(e.target.value);
+              setSearchTerm(value);
               if (value === "") {
                 setAppliedFilter("");
                 setFilter("");
               } else setFilter("");
             }}
           />
-          <button className="bg-purple-options text-gray-300 rounded-sm shadow w-[12%] cursor-pointer hover:scale-110">
+          <button
+            onClick={(e) => {
+              setSelectedAddStock(e);
+              setSelectedOption(options[0]);
+            }}
+            className="bg-purple-options text-gray-300 rounded-sm shadow w-[12%] cursor-pointer hover:scale-110"
+          >
             Entrada Estoque
           </button>
           <button className="bg-purple-options text-gray-300 rounded-sm shadow w-[12%] cursor-pointer hover:scale-110">
             Saida Estoque
           </button>
         </div>
-        <div className="relative w-[90%] h-91 overflow-y-auto bg-purple-items shadow-lg rounded-xl">
-          <div className="w-full max-h-91 overflow-auto">
-            <div className="grid grid-cols-4 bg-purple-itemsheader text-white font-bold sticky top-0 z-10">
-              <div className="px-4 py-2 border-r border-purple-border">
-                Tipo
-              </div>
-              <div className="px-4 py-2 border-r border-purple-border">
-                Sabor
-              </div>
-              <div className="px-4 py-2 border-r border-purple-border">
-                Quantidade
-              </div>
-            </div>
-            <div className="grid grid-rows-auto">
-              {filteredItems.map((item) => (
-                <div
-                  key={item.id}
-                  className="grid grid-cols-4 border-b border-purple-border text-white"
-                >
-                  <div className="px-4 py-2">{item.name}</div>
-                  <div className="px-4 py-2">{item.flavour}</div>
-                  <div className="px-4 py-2">{item.quantity}</div>
+        <div className="relative w-[90%] h-91 overflow-y-auto flex gap-4">
+          <div className="flex-1 bg-purple-items shadow-lg rounded-xl overflow-auto">
+            {filteredWithFlavour.length > 0 && (
+              <>
+                <div className="grid grid-cols-3 bg-purple-itemsheader text-white font-bold sticky top-0 z-10">
+                  <div className="px-4 py-2 border-r border-purple-border">
+                    Tipo
+                  </div>
+                  <div className="px-4 py-2 border-r border-purple-border">
+                    Sabor
+                  </div>
+                  <div className="px-4 py-2 border-r border-purple-border">
+                    Quantidade
+                  </div>
                 </div>
-              ))}
-            </div>
-            {selectedNewStock && (
-              <div className="fixed inset-0 bg-black/50 flex items-center justify-center z-50">
-                <div className="bg-gray-900 p-6 rounded-lg w-full max-w-md">
-                  <h2 className="text-purple-400 font-bold text-lg opacity-85">
-                    Dados do Estoque
-                  </h2>
-                  <ul className="space-y-2 py-5 flex flex-col gap-5">
-                    <select
-                      value={newType}
-                      onChange={(e) => {
-                        setNewType(e.target.value);
-                        setSelectedOption(
-                          options.find((o) => o.name === e.target.value),
-                        );
-                      }}
-                      className="border-b w-[40%] pl-2 text-white focus:bg-gray-900"
-                    >
-                      {options.map((o) => (
-                        <option key={o.code} value={o.name}>
-                          {o.name}
-                        </option>
-                      ))}
-                    </select>
-                    {selectedOption && !selectedOption.isDisabled && (
-                      <input
-                        type="text"
-                        className="text-white border-b border-gray-700 w-[40%] pl-2"
-                        placeholder="Sabor"
-                        value={newStock.flavour}
-                        onChange={(e) => {
-                          setNewStock({
-                            ...newStock,
-                            flavour: e.target.value,
-                          });
-                        }}
-                      />
-                    )}
-                    <input
-                      type="number"
-                      className="text-white border-b border-gray-700 w-[40%] pl-2"
-                      placeholder="Quantidade"
-                      value={newStock.quantity}
-                      onChange={(e) => {
-                        setNewStock({
-                          ...newStock,
-                          quantity: e.target.value,
-                        });
-                      }}
-                    />
-                    <div>
-                      <button
-                        onClick={addStock}
-                        className="bg-purple-800 text-gray-400 font-bold rounded-sm shadow px-5 flex justify-center items-center cursor-pointer hover:scale-110"
-                      >
-                        Cadastrar
-                      </button>
-                    </div>
-                  </ul>
-                  <button
-                    onClick={() => {
-                      setSelectedNewStock(null);
-                      setNewStock({
-                        type: "",
-                        flavour: "",
-                        quantity: "",
-                      });
-                    }}
-                    className="mt-4 w-full bg-purple-600 py-2 rounded cursor-pointer hover:scale-105"
+                {filteredWithFlavour.map((item) => (
+                  <div
+                    key={item.id}
+                    className="grid grid-cols-3 border-b border-purple-border text-white"
                   >
-                    Fechar
-                  </button>
+                    <div className="px-4 py-2">{item.name}</div>
+                    <div className="px-4 py-2">{item.flavour}</div>
+                    <div className="px-4 py-2">{item.quantity}</div>
+                  </div>
+                ))}
+              </>
+            )}
+          </div>
+          <div className="flex-1 bg-purple-items shadow-lg rounded-xl overflow-auto">
+            {filteredWithoutFlavour.length > 0 && (
+              <>
+                <div className="grid grid-cols-2 bg-purple-itemsheader text-white font-bold sticky top-0 z-10">
+                  <div className="px-4 py-2 border-r border-purple-border">
+                    Tipo
+                  </div>
+                  <div className="px-4 py-2 border-r border-purple-border">
+                    Quantidade
+                  </div>
                 </div>
-              </div>
+                {filteredWithoutFlavour.map((item) => (
+                  <div
+                    key={item.id}
+                    className="grid grid-cols-2 border-b border-purple-border text-white"
+                  >
+                    <div className="px-4 py-2">{item.name}</div>
+                    <div className="px-4 py-2">{item.quantity}</div>
+                  </div>
+                ))}
+              </>
             )}
           </div>
         </div>
+
+        {selectedNewStock && (
+          <div className="fixed inset-0 bg-black/50 flex items-center justify-center z-50">
+            <div className="bg-gray-900 p-6 rounded-lg w-full max-w-md">
+              <h2 className="text-purple-400 font-bold text-lg opacity-85">
+                Dados do Estoque
+              </h2>
+              <ul className="space-y-2 py-5 flex flex-col gap-5">
+                <select
+                  value={newType}
+                  onChange={(e) => {
+                    setNewType(e.target.value);
+                    setSelectedOption(
+                      options.find((o) => o.name === e.target.value),
+                    );
+                  }}
+                  className="border-b w-[40%] pl-2 text-white focus:bg-gray-900"
+                >
+                  {options.map((o) => (
+                    <option key={o.code} value={o.name}>
+                      {o.name}
+                    </option>
+                  ))}
+                </select>
+                {selectedOption && !selectedOption.isDisabled && (
+                  <input
+                    type="text"
+                    className="text-white border-b border-gray-700 w-[40%] pl-2"
+                    placeholder="Sabor"
+                    value={newStock.flavour}
+                    onChange={(e) =>
+                      setNewStock({ ...newStock, flavour: e.target.value })
+                    }
+                  />
+                )}
+                <input
+                  type="number"
+                  className="text-white border-b border-gray-700 w-[40%] pl-2"
+                  placeholder="Quantidade"
+                  value={newStock.quantity}
+                  onChange={(e) =>
+                    setNewStock({ ...newStock, quantity: e.target.value })
+                  }
+                />
+                <div>
+                  <button
+                    onClick={addStock}
+                    className="bg-purple-800 text-gray-400 font-bold rounded-sm shadow px-5 flex justify-center items-center cursor-pointer hover:scale-110"
+                  >
+                    Cadastrar
+                  </button>
+                </div>
+              </ul>
+              <button
+                onClick={() => {
+                  setSelectedNewStock(null);
+                  setNewStock({ type: "", flavour: "", quantity: "" });
+                }}
+                className="mt-4 w-full bg-purple-600 py-2 rounded cursor-pointer hover:scale-105"
+              >
+                Fechar
+              </button>
+            </div>
+          </div>
+        )}
+
+        {selectedAddStock && (
+          <div className="fixed inset-0 bg-black/50 flex items-center justify-center z-50">
+            <div className="bg-gray-900 p-5 rounded-lg w-full max-w-md max-h-[70vh] overflow-auto">
+              <div className="flex items-center justify-between pb-4">
+                <h2 className="text-white font-bold text-lg">
+                  Selecione o Produto
+                </h2>
+                <button
+                  onClick={() => {
+                    setSelectedAddStock(false);
+                    setSelectedItem(null);
+                    setNewQuantity("");
+                  }}
+                  className="text-white hover:bg-red-600"
+                >
+                  <X />
+                </button>
+              </div>
+
+              <div className="max-h-48 overflow-auto mb-4">
+                {items.map((item) => (
+                  <div
+                    key={item.id}
+                    onClick={() => setSelectedItem(item)}
+                    className={`p-3 mb-2 rounded cursor-pointer ${
+                      selectedItem?.id === item.id
+                        ? "bg-purple-600"
+                        : "bg-gray-800"
+                    }`}
+                  >
+                    <div className="text-gray-200">{item.name}</div>
+                    <div className="text-sm text-gray-300">
+                      {item.flavour || ""}
+                    </div>
+                    <div className="text-xs text-gray-400">
+                      Quantidade atual: {item.quantity}
+                    </div>
+                  </div>
+                ))}
+              </div>
+
+              {selectedItem && (
+                <>
+                  <label className="text-white block mb-2">
+                    Quantidade a adicionar:
+                  </label>
+                  <input
+                    type="number"
+                    min={1}
+                    value={newQuantity}
+                    onChange={(e) => setNewQuantity(e.target.value)}
+                    className="w-full p-2 rounded bg-gray-800 text-white mb-4"
+                  />
+                  <button className="w-full text-gray-300 bg-gray-700 py-2 rounded hover:scale-105 transition-transform">
+                    Adicionar Estoque
+                  </button>
+                </>
+              )}
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
